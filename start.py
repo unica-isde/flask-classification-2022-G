30ccea2b100f2256e836cd0b600ff02bb10d04c2
lmolinario <114803243+lmolinario@users.noreply.github.com>
2022-12-03 17:58:57 +0100

    inserting a single boot file

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import subprocess
 
+subprocess.Popen("sudo -s python worker.py", start_new_session=True, shell=True)
 
-subprocess.call("sudo -s python runserver.py", shell=True)
-subprocess.call("sudo -s python worker.py", shell=True)
-subprocess.call("sudo -s python worker_histo.py", shell=True)
+subprocess.Popen("sudo -s python worker_histo.py", start_new_session=True, shell=True)
+
+subprocess.Popen("sudo -s python runserver.py",start_new_session=True, shell=True)
