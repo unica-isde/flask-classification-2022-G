1bc2b667b062e00f6af0ab1a08185c44cf1fdf04
lmolinario <114803243+lmolinario@users.noreply.github.com>
2023-06-26 16:01:39 +0200

    Update test_redis.py
    Removed subprocess system call

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,17 +1,9 @@
 import unittest
 from config import Configuration
-import subprocess
 
 class TestRedis(unittest.TestCase):
     import redis
     config = Configuration()
     redis_url = config.REDIS_URL
     r = redis.from_url(redis_url)
-    try:
-        r.ping()
-    except Exception:
-        try:
-            subprocess.Popen("systemctl restart redis-server.service", shell=True)
-        except Exception:
-            subprocess.Popen("service redis-server start", shell=True)
-
+    r.ping()
